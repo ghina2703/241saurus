641a844bf6f358a072a42c18a58294f38a5905c0
Ghina Nabila Gunawan <124442361+ghina2703@users.noreply.github.com>
2024-04-04 10:05:52 +0700

    Inisialisasi gh-pages

--- Docusaurus/docusaurus.config.ts
+++ Docusaurus/docusaurus.config.ts
@@ -16,13 +16,9 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/241saurus',
-
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
